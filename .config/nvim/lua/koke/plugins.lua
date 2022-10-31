2be2e35d0c2cd7f8807b333911f6345b6aa1d87b
jmainhard <jmainhard@outlook.com>
2022-10-31 16:17:05 -0300

    fix telescope packer

--- .config/nvim/lua/koke/plugins.lua
+++ .config/nvim/lua/koke/plugins.lua
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
 	use 'nvim-tree/nvim-web-devicons'
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = {'nvim-lua/plenary.nvim'},
+		requires = { {'nvim-lua/plenary.nvim'} },
 	}
 	use {"akinsho/toggleterm.nvim", tag = '*', config = function()
 		require("toggleterm").setup()
